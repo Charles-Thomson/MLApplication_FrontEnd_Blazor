7fd3d8b3cc1c14ec2124143545b4d1bdcd2d7327
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2024-09-25 12:32:18 +1000

    Updating side nave to use expanding drop downs

---
+++ Components/Buttons/Buttons_Individual/IconButtonWithDropDown.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MachineLearningApplication_Build_2.Components.Buttons.Buttons_Individual
+{
+    public partial class IconButtonWithDropDown
+    { 
+        [Parameter] public string? ButtonText { get; set; }
+
+        public string HeightCSS { get; set; } 
+
+        public string Open = "300%";
+        public string Closed = "2rem";
+
+        public void OnClickHight() {
+            if (HeightCSS == Open)
+            {
+                HeightCSS = Closed;
+            }
+            else {
+                HeightCSS = Open;
+
+            }
+        }
+
+        public IconButtonWithDropDown() {
+            HeightCSS = Closed;
+        }
+
+    }
+}
