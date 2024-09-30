c21a60270baf608b7420f5ee921fec1d6939d42d
Charles.Steven.Thomson <Charles.Steven.Thomson@gmail.com>
2024-09-30 17:08:32 +1000

    Refactoring side menu - cleaner approach - need to remove dead code

--- Components/Buttons/Buttons_Individual/IconButtonWithDropDown.razor.cs
+++ Components/Buttons/Buttons_Individual/IconButtonWithDropDown.razor.cs
@@ -9,27 +9,53 @@ using System.Threading.Tasks;
 namespace MachineLearningApplication_Build_2.Components.Buttons.Buttons_Individual
 {
     public partial class IconButtonWithDropDown
-    { 
+    {
         [Parameter] public string? ButtonText { get; set; }
+        [Parameter] public string? Icon { get; set; }
 
-        public string HeightCSS { get; set; } 
+        public string HeightCSS { get; set; }
+        public string CloseButtonCSS { get; set; }
 
         public string Open = "300%";
         public string Closed = "2rem";
 
-        public void OnClickHight() {
+        public string Visable = "visible";
+        public string NotVisable = "hidden";
+
+        public string OnClickHight() {
             if (HeightCSS == Open)
             {
-                HeightCSS = Closed;
+                return Closed;
             }
             else {
-                HeightCSS = Open;
+                return Open;
 
             }
         }
 
+        public string CloseButtonVisibility() {
+            if (HeightCSS == Open)
+            {
+                return Visable;
+            }
+            else
+            {
+                return NotVisable;
+            }
+        }
+        
+
+        public void HandleOnclick() {
+
+            HeightCSS = OnClickHight();
+            CloseButtonCSS = CloseButtonVisibility();
+        }
+
         public IconButtonWithDropDown() {
             HeightCSS = Closed;
+            CloseButtonCSS = NotVisable;
+
+
         }
 
     }
